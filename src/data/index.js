5794b10abb738f3821463c8181e3c6997de92209
Jake Gealer <jake@gealer.email>
2020-04-13 15:33:16 +0100

    Revert "hmmmmmm"
    This reverts commit 88d58aa67be4557bc5e6db2fa6b1574969e45fbe.

--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,7 @@
-import diagrams from './diagrams.js';
-import connectors from './connectors.js';
-import nodes from './nodes.ts';
-import documentation from './documentation.js';
+import diagrams from './diagrams';
+import connectors from './connectors';
+import nodes from './nodes';
+import documentation from './documentation';
 import * as additionalIcons from './additionalIcons';
 
 export default {
