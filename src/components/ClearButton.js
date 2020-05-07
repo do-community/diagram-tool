f8b518dc5d16cf1a21cb67aed45e0feaa34d0ebf
Jake Gealer <jake@gealer.email>
2020-05-07 16:23:33 +0100

    fix quotes (sorry ESLint)

--- src/components/ClearButton.js
+++ src/components/ClearButton.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
 const clear = () => {
-    localStorage.removeItem("infragramState");
-    window.location.replace("/");
+    localStorage.removeItem('infragramState');
+    window.location.replace('/');
     window.location.reload();
 };
 
 export default class ClearButton extends React.Component {
     render() {
-        return <span style={{position: "absolute"}} className="do-bulma infragram">
+        return <span style={{position: 'absolute'}} className="do-bulma infragram">
             <a className="button is-info" onClick={clear}>
                 Clear
             </a>
