1142f3ea0742133efb7bdbd5e9d4ff4734e5e1d8
Jake Gealer <jake@gealer.email>
2020-05-19 15:33:37 +0100

    Add license headers

--- src/data/additionalIcons/bubble.tsx
+++ src/data/additionalIcons/bubble.tsx
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 DigitalOcean
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 import * as React from "react";
 
 export default <path className="waxpaper" d="M50,1c27.062,0,49,21.938,49,49S77.062,99,50,99 C22.938,99,1,77.063,1,50S22.938,1,50,1z" />;
